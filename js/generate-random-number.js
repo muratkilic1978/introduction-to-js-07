6d3ea662465225037a874edcd5d3af7f4dd899c6
muratkilic1978 <muratkilic1978@gmail.com>
2020-02-05 21:02:50 +0100

    script.js was updated

--- js/generate-random-number.js
+++ js/generate-random-number.js
@@ -1,12 +1,2 @@
-const showNumber = document.getElementById("showNumber");
-const btnNumber = document.querySelector(".btnNumber");
-btnNumber.addEventListener("click",function(){
-    
-    let randomNumber =  Math.floor((Math.random() * 100) +1);
-    console.log(randomNumber);
-    showNumber.innerHTML = randomNumber;
-});
-
-
 
 
